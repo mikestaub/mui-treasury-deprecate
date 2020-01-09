29527a8e8fbe741973c3f31daa132ecd81c6cf83
Dung Nguyen <attactive.man@gmail.com>
2020-01-08 19:24:08 -0800

    make buttons animate with slider

--- src/brands/peapods/lib/PeaThreadTitle.js
+++ src/brands/peapods/lib/PeaThreadTitle.js
@@ -5,7 +5,6 @@ import { makeStyles } from '@material-ui/styles';
 
 import PeaAvatar from './PeaAvatar';
 import PeaText from './PeaTypography';
-import PeaButton from './PeaButton';
 
 const useStyles = makeStyles(({ spacing }) => ({
   root: {
@@ -27,18 +26,12 @@ const useStyles = makeStyles(({ spacing }) => ({
   },
   avatarGroupContainer: {
     display: 'flex',
-    justifyContent: 'space-between',
-    padding: spacing(2),
+    justifyContent: 'center',
+    paddingBottom: spacing(2),
   },
 }));
 
-const PeaThreadTitle = ({
-  title,
-  subtitle,
-  avatars,
-  isPeasShown,
-  showPeas,
-}) => {
+const PeaThreadTitle = ({ title, subtitle, avatars }) => {
   const classes = useStyles();
 
   const maxAvatarCount = 5;
@@ -71,27 +64,6 @@ const PeaThreadTitle = ({
                 : undefined
             }
           />
-          {title &&
-            (isPeasShown ? (
-              <PeaButton
-                variant="contained"
-                color="secondary"
-                icon="fas fa-arrow-left"
-                size="small"
-                onClick={showPeas}
-              >
-                Back
-              </PeaButton>
-            ) : (
-              <PeaButton
-                variant="contained"
-                color="secondary"
-                size="small"
-                onClick={showPeas}
-              >
-                View Peas
-              </PeaButton>
-            ))}
         </div>
       </Grid>
     </Grid>
@@ -102,16 +74,12 @@ PeaThreadTitle.propTypes = {
   title: PropTypes.string,
   subtitle: PropTypes.string,
   avatars: PropTypes.arrayOf(PropTypes.string),
-  isPeasShown: PropTypes.bool,
-  showPeas: PropTypes.func,
 };
 
 PeaThreadTitle.defaultProps = {
   title: 'Unknown',
   subtitle: 'unknown',
   avatars: [],
-  isPeasShown: false,
-  showPeas: () => {},
 };
 
 PeaThreadTitle.metadata = {

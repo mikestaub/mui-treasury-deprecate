ed7536cdb2f38abc6e93fca4ff9869189e1c0e8f
Karan Pratap Singh <synester.gates4@gmail.com>
2020-06-09 14:52:57 +0530

    updated connection demo

--- src/brands/peapods/demo/Connections.js
+++ src/brands/peapods/demo/Connections.js
@@ -4,11 +4,17 @@ import { SOCIAL1, SOCIAL2, SOCIAL3 } from './_mock';
 
 const Connections = () => (
   <PeaConnections
+    allowToConnect
     followers={SOCIAL1}
-    following={SOCIAL1}
-    tags={SOCIAL2}
-    groups={SOCIAL3}
-    onClose={() => alert('close!')}
+    following={SOCIAL2}
+    friends={SOCIAL3}
+    interests={SOCIAL1}
+    connectionsCount={{
+      followers: SOCIAL1.length,
+      following: SOCIAL2.length,
+      friends: SOCIAL3.length,
+      interests: SOCIAL1.length,
+    }}
   />
 );
 
@@ -18,66 +24,85 @@ Connections.metadata = {
 Connections.codeSandbox = 'https://codesandbox.io/s/zljn06jmq4';
 Connections.code = `
   import PeaConnections from '../lib/PeaConnections';
-  
+
   const SOCIAL1 = [
     {
       src: 'https://avatars.dicebear.com/v2/avataaars/example.svg',
       name: 'Yeoman',
+      username: 'Yeoman',
+      unique: 'Yeoman',
       social: 'twitter',
     },
     {
       src: 'https://avatars.dicebear.com/v2/avataaars/example2.svg',
       name: 'GGWP',
+      username: 'GGWP',
+      unique: 'GGWP',
       social: 'meetup',
     },
     {
       src: 'https://avatars.dicebear.com/v2/avataaars/example3.svg',
       name: 'Sawasdee',
+      username: 'Sawasdee',
+      unique: 'Sawasdee',
       social: 'facebook',
     },
   ];
+
   const SOCIAL2 = [
     {
       src: 'https://avatars.dicebear.com/v2/avataaars/example4.svg',
       name: 'Yeoman',
-      social: 'tag',
+      username: 'Yeoman',
+      unique: 'Yeoman',
+      social: 'twitter',
     },
     {
       src: 'https://avatars.dicebear.com/v2/avataaars/example5.svg',
       name: 'GGWP',
-      social: 'tag',
+      username: 'GGWP',
+      unique: 'GGWP',
+      social: 'meetup',
     },
   ];
+
   const SOCIAL3 = [
     {
       src: 'https://avatars.dicebear.com/v2/avataaars/example7.svg',
       name: 'Yeoman',
-      social: 'group',
+      username: 'Yeoman',
+      unique: 'Yeoman',
+      social: 'twitter',
     },
     {
       src: 'https://avatars.dicebear.com/v2/avataaars/example8.svg',
       name: 'GGWP',
-      social: 'group',
+      username: 'GGWP',
+      unique: 'GGWP',
+      social: 'meetup',
     },
     {
       src: 'https://avatars.dicebear.com/v2/avataaars/example9.svg',
       name: 'Sawasdee',
-      social: 'group',
-    },
-    {
-      src: 'https://avatars.dicebear.com/v2/avataaars/example10.svg',
-      name: 'Test',
-      social: 'group',
+      username: 'Sawasdee',
+      unique: 'Sawasdee',
+      social: 'facebook',
     },
   ];
   
   const Preview = () => (
     <PeaConnections
+      allowToConnect
       followers={SOCIAL1}
-      following={SOCIAL1}
-      tags={SOCIAL2}
-      groups={SOCIAL3}
-      onClose={() => alert('close!')}
+      following={SOCIAL2}
+      friends={SOCIAL3}
+      interests={SOCIAL1}
+      connectionsCount={{
+        followers: SOCIAL1.length,
+        following: SOCIAL2.length,
+        friends: SOCIAL3.length,
+        interests: SOCIAL1.length,
+      }}
     />
   )
 `;
